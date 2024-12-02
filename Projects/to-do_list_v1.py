1df9bd8d960c998ba9d5c721675bcd13d94ddd00
Joao Victor Franca <joao.victor.franca.new@gmail.com>
2024-12-01 20:49:08 -0800

    Re made the show function and complete task function

--- Projects/to-do_list_v1.py
+++ Projects/to-do_list_v1.py
@@ -1,4 +1,5 @@
 from datetime import date
+
 to_do_list = {
     "ID" : [] ,
     "Description" : [] ,
@@ -11,25 +12,45 @@ id_counter = 0
 counter = 0
 
 def add_item():
+    global id_counter
+    id_counter += 1
     item_to_be_added = input("Enter the item you want to be added to the list: ")
-    to_do_list["ID"].append(id_counter)
-    to_do_list["Description"].append(item_to_be_added)
-    to_do_list['Status'].append("todo")
-    to_do_list['CreatedAt'].append(date.today())
+    to_do_list["ID"]+=[id_counter]
+    to_do_list["Description"]+=[item_to_be_added]
+    to_do_list['Status']+=["todo"]
+    to_do_list['CreatedAt']+=[date.today()]
+    to_do_list["UpdatedAt"]+=[date.today()]
 
-def complete_task():
-    taks_to_be_completed = int(input("Insert the number of the task you want to be completed: "))
-    try:
-        to_do_list.update({"Status" : "Done"})
-        to_do_list.update({"UpdatedAt" : date.today()})
-    except:
-        print("an error in deleting the item has occured")
+
+def update_status():
+    which_task = int(input("ID of the task to be updated \n>"))
+    if which_task > len(to_do_list["ID"]) or which_task <= 0:
+        print("Not valid")
+        return
+    #I'm tired so I'm going to do the lame way :)
+    update_counter = 0
+    for n in range(len(to_do_list["ID"])):
+        if update_counter == which_task-1:
+            print(f'''ID: {to_do_list["ID"][n]}
+                    Task: {to_do_list["Description"][n]}
+                    Status: {to_do_list["Status"][n]}
+                    Created at {to_do_list["CreatedAt"][n]}
+                    Last updated at {to_do_list["UpdatedAt"][n]}
+                --------------------------------------''')
+        update_counter+=1
+    
 
 def show_to_do_list():
-    counter = 0
-    print(to_do_list)
-
+    #Function to show the dictionary, pretty way of doing it but could't think on other way of doing
+    for n in range(len(to_do_list["ID"])):
+        print(f'''ID: {to_do_list["ID"][n]}
+                Task: {to_do_list["Description"][n]}
+                Status: {to_do_list["Status"][n]}
+                Created at {to_do_list["CreatedAt"][n]}
+                Last updated at {to_do_list["UpdatedAt"][n]}
+              --------------------------------------''')
     
+
 while True:
     if counter == 0:
         add_item()
@@ -37,13 +58,13 @@ while True:
     else:
         show_to_do_list
         choice = input(">")
-        if choice == "list.add":
+        if choice == "add":
             add_item()
-        elif choice == "list.complete":
-            complete_task()
-        elif choice == "list.show":
+        elif choice == "update":
+            update_status()
+        elif choice == "show":
             show_to_do_list()
-        elif choice == "list.quit":
+        elif choice == "quit":
             break
         else:
             print("not a valid command")
